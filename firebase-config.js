90cfaaffaac04863b5dd82d352982e0994729208
Faiz Hassany <syedfaizhassany@gmail.com>
2026-08-27 15:58:18 -0700

    fixed firebase config bug

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// ====================================================================
 const firebaseConfig = {
   apiKey: "AIzaSyBS2qiWyjIc1XrW6n6oGCPHfnfeAz6zB9Q",
   authDomain: "resolve-rehab-queue.firebaseapp.com",
@@ -15,7 +9,10 @@ const firebaseConfig = {
   appId: "1:8080983677:web:9a657d98b3ba8d51399314",
   measurementId: "G-JXVLMH2Y72"
 };
+// ====================================================================
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
